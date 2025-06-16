39b7ed60a04e579970604cc721fe8f746fb4dfeb
aui19f <aui19f@gmail.com>
2025-06-16 22:07:25 +0900

    다시 정리해서 개발 시작

--- app/master/[id]/actions.ts
+++
@@ -1,9 +0,0 @@
-// "use server";
-// import db from "@/lib/db";
-// export default async function getItems(id: string) {
-//   // return await db.receive.findUnique({
-//   //   where: {
-//   //     id,
-//   //   },
-//   // });
-// }
