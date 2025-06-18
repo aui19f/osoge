8b25d03a2a62fa0ca0619e4bfb2133a6a1024299
aui19f <aui19f@gmail.com>
2025-06-18 20:44:33 +0900

    타이틀 변경, 접수

--- app/master/layout.tsx
+++ app/master/layout.tsx
@@ -3,24 +3,37 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function MasterLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const [title, setTitle] = useState("어서오게(OSOGE)");
   const pathname = usePathname();
-
   const masterMenu = [
-    { name: "menu_home", path: "/master" },
-    { name: "menu_list", path: "/master/list" },
-    { name: "menu_search", path: "/master/search" },
-    { name: "menu_registration", path: "/master/receive" },
-    { name: "menu_settings", path: "/master/settings" },
+    { name: "menu_home", path: "/master", title: "어서오게(OSOGE)" },
+    { name: "menu_list", path: "/master/list", title: "목록" },
+    { name: "menu_search", path: "/master/search", title: "검색" },
+    { name: "menu_registration", path: "/master/receive", title: "접수" },
+    { name: "menu_settings", path: "/master/settings", title: "설정" },
   ];
+  // const current = masterMenu.find((menu) => {
+  //   console.log("menu.path", menu.path);
+  //   return pathname.startsWith(menu.path);
+  // });
+  // const title = current?.title || "어서오게(OSOGE)";
+
+  useEffect(() => {
+    const current = masterMenu
+      .sort((a, b) => b.path.length - a.path.length)
+      .find((menu) => pathname.startsWith(menu.path));
+    setTitle(current?.title || "기본 타이틀");
+  }, [pathname]); // pathname이 바뀔 때마다 실행됨
 
   return (
     <div>
       <div className="flex items-center justify-center h-20 bg-slate-50">
-        <h2 className="text-3xl font-bold text-slate-400">TITLE</h2>
+        <h2 className="text-2xl font-bold text-slate-400">{title}</h2>
       </div>
       <main className="h-[calc(100vh-160px)] overflow-auto">{children}</main>
       <footer className="flex h-20">
