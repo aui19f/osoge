1086338927d5858643a0b8d5492f43859994c213
aui19f <aui19f@gmail.com>
2025-06-25 19:23:30 +0900

    build수정

--- lib/sesstion.ts
+++ lib/sesstion.ts
@@ -10,23 +10,6 @@ interface ISessionContent {
 export default async function getSession() {
   return getIronSession<ISessionContent>(await cookies(), {
     cookieName: "osoge",
-    password: process.env.COOKIE_PASSWORD!,
+    password: process.env.NEXT_PUBLIC_COOKIE_PASSWORD!,
   });
 }
-
-// const sesstion = await getSession();
-// session.id = user.id;
-// await session.save();
-
-// import { getIronSession } from "iron-session";
-// import { cookies } from "next/headers";
-// interface ISesstionContent {
-//   id?: string;
-// }
-
-// export default async function getSesstion() {
-//   return getIronSession<ISesstionContent>(await cookies(), {
-//     cookieName: "karrot",
-//     password: process.env.COOKIE_PASSWORD!,
-//   });
-// }
--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,13 +1,3 @@
-// // lib/supabase.ts
-// import { createBrowserClient } from "@supabase/auth-helpers-nextjs";
-// export const createClient = () =>
-//   createBrowserClient(
-//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-//   );
-
-// Supabase 클라이언트 설정 (lib 폴더 만들기)
-
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
