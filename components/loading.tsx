65e9edbf4a0aafa5a7e0baad0721d7f40306cabf
aui9f <aui19f@gmail.com>
2025-10-18 12:22:18 +0900

    구조수정중, 사용하지 않으면 우선 삭제2

--- components/loading.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function Loading() {
-  return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900/40">
-      <div className="w-16 h-16 border-4 border-white rounded-full animate-spin border-t-transparent"></div>
-    </div>
-  );
-}
